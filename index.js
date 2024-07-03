bad32b3056231f35e5effe9b7785ae0315e7b01b
markjoe0304 <markjoegicheru@gmail.com>
2024-07-03 08:55:01 +0300

    moded

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+let num1 =1;
+let num2 =62;
+let multiply =num1*num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 =50;
+let num4 =46;
+let mod =num3%num4;
+let max= Math.max(15,16,17,18,19,20)
